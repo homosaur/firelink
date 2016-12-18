b2db39db169d60a182a34af3245d862dad5e64de
Paul Graham <homosaur@gmail.com>
2016-12-18 00:44:03 -0600

    refactor tests to test functions rather than API

--- test/component.js
+++ test/component.js
@@ -1,54 +1,30 @@
-const supertest = require('supertest')
-const server = supertest.agent('http://localhost:9001')
-
 const should = require('chai').should() // eslint-disable-line
+const db = require('../api/db')
 
 // created testId
 let testId
 
 before(() => {
-  // boot()
 })
 
 describe('component', () => {
-  it('should create a component on /api/component/create', (done) => {
-    server
-      .get('/api/component/create')
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err
-        res.status.should.equal(200)
-        testId = res.text
-        testId.should.have.length(9)
-        done()
-      })
+  it('should create a component', () => {
+    const components = db.create('components')
+    testId = components[components.length - 1].id
+    testId.should.have.length(9)
   })
 
-  it('should return an existing object at /api/component/find/:id', (done) => {
-    server
-      .get(`/api/component/find/${testId}`)
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err
-        res.status.should.equal(200)
-        res.body.should.not.be.empty
-        res.body.id.should.have.length(9)
-        done()
-      })
+  it('should return an existing object using id', () => {
+    const dbObject = db.find('components', testId)
+    dbObject.should.not.be.empty
+    dbObject.id.should.have.length(9)
   })
 
-  it('should return a 404 upon nonexistant object at /api/component/find/:id', (done) => {
-    server
-      .get('/api/component/find/invalidstring')
-      .expect(404)
-      .end((err, res) => {
-        if (err) throw err
-        res.status.should.equal(404)
-        done()
-      })
+  it('should return a 404 upon nonexistant object using invalid id', () => {
+    const dbObject = db.find('components', 'invalidId')
+    dbObject.should.be.false
   })
 })
 
 after(() => {
-  // shutdown()
 })

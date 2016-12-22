5f0d778f9aa776b16213ed3724872288d00d3c8d
Paul Graham <homosaur@gmail.com>
2016-12-22 15:25:25 -0600

    es6ify tests as well as switch to expect style

--- test/component.js
+++ test/component.js
@@ -1,5 +1,5 @@
-const should = require('chai').should() // eslint-disable-line
-const db = require('../api/db')
+import {expect} from 'chai'
+import * as db from '../api/db'
 
 // created testId
 let testId
@@ -11,18 +11,18 @@ describe('component', () => {
   it('should create a component', () => {
     const components = db.create('components')
     testId = components[components.length - 1].id
-    testId.should.have.length(9)
+    expect(testId).to.have.length(9)
   })
 
   it('should return an existing object using id', () => {
     const dbObject = db.find('components', testId)
-    dbObject.should.not.be.empty
-    dbObject.id.should.have.length(9)
+    expect(dbObject).to.not.be.empty
+    expect(dbObject.id).to.have.length(9)
   })
 
   it('should return a 404 upon nonexistant object using invalid id', () => {
     const dbObject = db.find('components', 'invalidId')
-    dbObject.should.be.false
+    expect(dbObject).to.be.false
   })
 })
 
